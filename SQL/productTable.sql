fd2377a644800d33e2af2ac0c8fb339ab0e84951
BeachBabe-King <jessiekd2000@gmail.com>
2025-11-07 16:42:37 -0500

    table creation code

---
+++ SQL/productTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE FPproduct(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    stock INT DEFAULT 0 CHECK (stock >=0),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
