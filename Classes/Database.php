05af9f6e69f2bd985885508518726b682183df38
BeachBabe-King <jessiekd2000@gmail.com>
2025-11-07 17:30:56 -0500

    database connection and config complete

--- Classes/Database.php
+++ Classes/Database.php
@@ -1,1 +1,31 @@
 <?php
+//Database connection
+class Database
+{
+    // private properties store database connection info
+    private $host = DB_HOST;
+    private $db = DB_NAME;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $pdo;
+
+    public function getConnection()
+    {
+        //creates connection if one doesnt exist
+        if ($this->pdo === null) {
+            try {
+                $dsn = "mysql:host={$this->host};dbname={$this->db};charset=utf8mb4";
+                $this->pdo = new PDO($dsn, $this->user, $this->pass);
+                //sets pdo to throw exceptions
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //return row as array
+                $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            } catch (PDOException $e) {
+                die("Could not connect to the database: " . $e->getMessage());
+            }
+        }
+        return $this->pdo;
+    }
+}
+?>
