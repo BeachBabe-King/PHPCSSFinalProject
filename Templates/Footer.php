4147ae98d887536b2110cb5d838e78a1587b6f9b
BeachBabe-King <jessiekd2000@gmail.com>
2025-11-08 17:22:58 -0500

    color pallet, footer and header templates made

--- Templates/Footer.php
+++ Templates/Footer.php
@@ -1,2 +1,56 @@
-<!-- Header template -->
-<?php
+<!-- Footer template -->
+</main>
+
+<footer>
+    <!-- Left footer content container -->
+    <div class="footContainer">
+        <!-- List columns -->
+        <div class="footColumn">
+            <!-- Contents lists (temporarily blank) -->
+            <ul class="footColumnList">
+                <li><a href="#">Shipping & Returns</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Find a Store</a></li>
+                <li><a href="#">Provide Feedback</a></li>
+                <li><a href="#">Careers</a></li>
+            </ul>
+        </div>
+        <div class="footColumn">
+            <ul class="footColumnList">
+                <li><a href="#">Monthly Reading Challenge</a></li>
+                <li><a href="#">Donations</a></li>
+                <li><a href="#">Recycling Program</a></li>
+                <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+        <div class="footColumn">
+            <ul class="footColumnList">
+                <li><a href="#">Publishers</a></li>
+                <li><a href="#">Authors</a></li>
+                <li><a href="#">Recalls</a></li>
+                <li><a href="#">Become a Vendor</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Right side footer container -->
+    <div class="footContainer">
+        <h3>Join Infinite Rewards Club</h3>
+        <p>Collect rewards on every purchase and redeem on book bucks, shipping, and more</p>
+        <a class="button" href="#">Learn More</a>
+        <div class="socMediaLinks">
+            <a href="#">
+                <img src="Assets/Icons/facebook.png" alt="Facebook Logo"></a>
+            <a href="#">
+                <img src="Assets/Icons/instagram.png" alt="Instagram Logo"></a>
+            <a href="#">
+                <img src="Assets/Icons/twitter.png" alt="X Logo"></a>
+        </div>
+    </div>
+    <div class="footerBottom">
+        <p>&copy; <?php echo date('Y'); ?> The Infinite Library. All Rights Reserved </p>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Preferences</a>
+        <a href="#">Terms of Use</a>
+    </div>
+</footer>
--- Templates/Header.php
+++ Templates/Header.php
@@ -1,2 +1,26 @@
-<!-- Header template -->
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Profile creator page">
+    <meta name="author" content="Jessie Davis 200433256">
+    <title><?php echo $pageTitle ?? 'The Infinite Library'; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <nav class="headNav">
+        <div class="logo">
+            <a href="/index.php">
+                <img src="Assets/Images/TheInfiniteLibraryLogo.png" alt="The Infinite Library Logo"></a>
+        </div>
+        <ul class="navLinks">
+            <li><a href="Shop.php">Browse</a></li>
+            <li><a href="About.php">About</a></li>
+            <li><a href="Contact.php">Contact</a></li>
+            <li><a href="RegLog.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
+<main class="mainContent">
