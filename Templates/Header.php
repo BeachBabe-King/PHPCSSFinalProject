b1257e57f5ae7c6351c7525037aba4f31deedce0
BeachBabe-King <jessiekd2000@gmail.com>
2025-11-09 15:55:00 -0500

    minor styling for header, added more to the header as it was a bit barebones

--- Templates/Header.php
+++ Templates/Header.php
@@ -10,16 +10,45 @@
 </head>
 <body>
 <header>
-    <nav class="headNav">
+    <!-- Top part of header -->
+    <div class="headerTop">
+        <!-- clickable logo -->
         <div class="logo">
-            <a href="/index.php">
+            <a href="index.php">
                 <img src="Assets/Images/TheInfiniteLibraryLogo.png" alt="The Infinite Library Logo"></a>
         </div>
+        <!-- Search bar -->
+        <div class="searchBar">
+            <form action="Shop.php" method="GET" class="searchForm">
+                <input type="search" name="searchBar" placeholder="Search your next read!">
+                <button type="submit" class="searchBtn">
+                    <img src="Assets/Icons/magGlassIcon.svg" alt="Search" class="icon">
+                </button>
+            </form>
+        </div>
+        <!-- Fav Acc Cart -->
+        <div class="headUserItems">
+            <!-- Fav -->
+            <a href="#" class="linkIcon">
+                <img src="Assets/Icons/heartIcon.svg" alt="Favourites" class="Icon"></a>
+            <!-- Acc -->
+            <a href="#" class="linkIcon">
+                <img src="Assets/Icons/Account.svg" alt="Account" class="Icon"></a>
+            <!-- Cart -->
+            <a href="#" class="linkIcon">
+                <img src="Assets/Icons/cartIcon.svg" alt="Cart" class="Icon"></a>
+        </div>
+        <!-- Login button -->
+        <div class="loginBtn">
+            <a href="RegLog.php" class="btn">Login</a>
+        </div>
+    </div>
+    <!-- nav under search and user actions -->
+    <nav class="headNav">
         <ul class="navLinks">
             <li><a href="Shop.php">Browse</a></li>
             <li><a href="About.php">About</a></li>
             <li><a href="Contact.php">Contact</a></li>
-            <li><a href="RegLog.php">Login</a></li>
         </ul>
     </nav>
 </header>
