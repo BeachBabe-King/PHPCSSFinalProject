df06a4aebd87ef12c919ab44fe8586685e32b3b6
BeachBabe-King <jessiekd2000@gmail.com>
2025-11-15 11:12:57 -0500

    Changed admin table

--- SQL/adminTable.sql
+++ SQL/adminTable.sql
@@ -1,7 +1,11 @@
-CREATE TABLE FPadmin(
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(100) NOT NULL,
-                        email VARCHAR(100) UNIQUE NOT NULL,
-                        password VARCHAR(255) NOT NULL,
-                        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE FPAdminTable (
+                       userId INT AUTO_INCREMENT PRIMARY KEY,
+                       firstName VARCHAR(50) NOT NULL,
+                       lastName VARCHAR(50) NOT NULL,
+                       email VARCHAR(100) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       phone VARCHAR(20) DEFAULT NULL,
+                       address TEXT DEFAULT NULL,
+                       isAdmin BOOLEAN DEFAULT FALSE,
+                       createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
